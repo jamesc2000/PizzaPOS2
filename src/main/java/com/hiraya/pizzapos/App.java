f0392948f7e95e6ebad56bb627d8e34b92edd2a8
James Cruz <cruz.james99@gmail.com>
2021-11-27 11:19:17 +0800

    Added logic for success/fail firebase login

--- src/main/java/com/hiraya/pizzapos/App.java
+++ src/main/java/com/hiraya/pizzapos/App.java
@@ -14,6 +14,9 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    // This loggedInUser has the same lifetime as the app itself
+    // You can access this in other classes through App.user.<attribute/method>
+    public CurrentUser user = new CurrentUser();
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -38,8 +41,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        CurrentUser loggedInUser = new CurrentUser(); // At the very start of app, initialize a blank user
-        // This loggedInUser has the same lifetime as the app itself
         launch();
     }
 }
--- src/main/java/com/hiraya/pizzapos/login/LoginController.java
+++ src/main/java/com/hiraya/pizzapos/login/LoginController.java
@@ -1,8 +1,10 @@
 package com.hiraya.pizzapos.login;
 
 import com.hiraya.pizzapos.App;
+import com.hiraya.pizzapos.httpReqRes.LoginResponse;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
@@ -26,7 +28,7 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    public void login() {
+    public void login() throws IOException {
         System.out.println("Login clicked");
         
         // When login is clicked, store the data to the model
@@ -35,7 +37,41 @@ public class LoginController implements Initializable {
 
         // Send the email and password to firebase, then firebase is
         // responsible for verifying credentials, not us
-        model.sendToFirebase();
+        LoginResponse response = model.sendToFirebase();
+        if (Objects.isNull(response.error)) {
+            // If no errors, proceed with handshake, exchange login refreshToken
+            // for accessTokens and idTokens
+            
+            // Boss CJ insert mo dito code for the handshake hehe
+
+            // After the handshake is complete, put the tokens to the actual User
+            // Uncomment line below when handshake logic is complete
+            //App.user.setTokens(refreshToken, idToken, accessToken);
+            
+            System.out.println("Log in successful");
+            // Display toast notif here saying "Log in successful"
+            App.setRoot("takeOrders");
+        } else {
+            // If there are errors, show a notification to the user containing the message
+            // Common error codes for firebase auth API found here:
+            // https://firebase.google.com/docs/reference/rest/auth/#section-sign-in-email-password
+            if (response.error.message.equals("EMAIL_NOT_FOUND")) {
+                System.out.println("Email not registered!");
+                // Display toast notif saying "Email not registered"
+                // TODO: Make a wrapper function to spawn toast notifications in UI
+            } else if (response.error.message.equals("INVALID_PASSWORD")) {
+                System.out.println("Invalid password!");
+                // Same thing for notifs here
+            } else if (response.error.message.equals("USER_DISABLED")) {
+                System.out.println("User has been disabled by administrator.");
+                // Same thing for notifs here
+            } else {
+                // If the error message from API is unrecognized, just display it
+                // with the ugly format as is
+                System.out.println(response.error.message);
+                // Display a notif still so user can be aware
+            }
+        }
     }
     
     @FXML
